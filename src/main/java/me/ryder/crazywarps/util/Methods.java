ad6d02124a8f6ea85556866c721cf83410122316
Ryder Belserion <Kolt0517@gmail.com>
2019-07-10 17:29:29 -0400

    added another string

--- src/main/java/me/ryder/crazywarps/util/Methods.java
+++ src/main/java/me/ryder/crazywarps/util/Methods.java
@@ -24,4 +24,12 @@ public class Methods {
     public static String getPerms() {
         return getPerms("");
     }
+
+    public static String getReload(String string) {
+        return pl(FileManager.Files.LANG.getFile().getString("messages.reload") + string);
+    }
+
+    public static String getReload() {
+        return getReload();
+    }
 }
