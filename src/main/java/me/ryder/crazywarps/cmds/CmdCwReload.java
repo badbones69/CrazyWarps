226505b7fe13e9afa05b28e6422f7159bbbee5d7
Ryder Belserion <Kolt0517@gmail.com>
2019-07-11 18:15:05 -0400

    still broken

--- src/main/java/me/ryder/crazywarps/cmds/CmdCwReload.java
+++ src/main/java/me/ryder/crazywarps/cmds/CmdCwReload.java
@@ -1,30 +1,31 @@
 package me.ryder.crazywarps.cmds;
 
 import me.ryder.crazywarps.util.Methods;
-import me.ryder.crazywarps.util.fm.FileManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CmdCwReload implements CommandExecutor {
-
+    @Override
     public boolean onCommand(CommandSender p, Command cmd, String label, String[] args) {
+        if (args.length == 0) {
 
-        if (label.equalsIgnoreCase("cwr")) {
-            if (!p.hasPermission("cw.access")) {
+            if (p.hasPermission("cw.access")) {
                 p.sendMessage(Methods.getPrefix() + Methods.getPerms());
                 return true;
             }
 
-            else {
-                p.sendMessage(Methods.getPrefix() + Methods.getReload(""));
-                FileManager.Files.CONFIG.reloadFile();
-                FileManager.Files.LANG.reloadFile();
-                FileManager.Files.DATA.reloadFile();
+            if (!(p instanceof Player)) {
+                p.sendMessage(Methods.pl("&c[&e!&c] This command can only be ran by a player."));
+                return true;
+            }
+
+        } else {
+            if (args[0].equalsIgnoreCase("reload")) {
+                p.sendMessage(Methods.getPrefix() + Methods.getReload());
             }
         }
-
         return true;
     }
-
 }
