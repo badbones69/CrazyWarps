fcc6399aa0143756012b64936157db14ba8cff6b
Ryder Belserion <Kolt0517@gmail.com>
2019-07-13 18:07:30 -0400

    setwarp & reload command

--- src/main/java/me/ryder/crazywarps/cmds/Commands.java
+++ src/main/java/me/ryder/crazywarps/cmds/Commands.java
@@ -1,15 +1,16 @@
 package me.ryder.crazywarps.cmds;
 
+import me.ryder.crazywarps.CrazyWarps;
 import me.ryder.crazywarps.util.Methods;
-import me.ryder.crazywarps.util.fm.FileManager;
+import me.ryder.crazywarps.util.fm.FileManager.Files;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 public class Commands implements CommandExecutor {
-
     @Override
     public boolean onCommand(CommandSender p, Command cmd, String label, String[] args) {
         if (args.length == 0) {
@@ -24,7 +25,7 @@ public class Commands implements CommandExecutor {
             p.sendMessage("");
             p.sendMessage(Methods.pl("&8- &c/cw reload &7This will reload the plugin."));
             p.sendMessage(Methods.pl("&8- &c/cw delwarp <warp name> &7This will delete a warp."));
-            p.sendMessage(Methods.pl("&8- &c/cw setwarp <warp name> &7This will set a warp."));
+            p.sendMessage(Methods.pl("&8- &c/cw setwarp <category> <warp name> &7This will set a warp."));
             p.sendMessage(Methods.pl("&8- &c/cw warplist &7This will list all warps and categories."));
             p.sendMessage(Methods.pl("&8- &c/cw warpsync &7This will sync Essentials & CMI warps into CrazyWarps."));
             return true;
@@ -33,36 +34,47 @@ public class Commands implements CommandExecutor {
                 if (!Methods.hasPermission(p, "reload", true)) {
                     return true;
                 }
-                FileManager.Files.LANG.reloadFile();
-                FileManager.Files.CONFIG.reloadFile();
-                FileManager.Files.DATA.reloadFile();
+                Files.LANG.reloadFile();
+                Files.CONFIG.reloadFile();
+                Files.DATA.reloadFile();
                 p.sendMessage(Methods.getPrefix() + Methods.getReload());
                 return true;
             }
-
             if (args[0].equalsIgnoreCase("setwarp")) {
-                final Player target = (Player) p;
-                Location loc = target.getLocation();
-                String world = loc.getWorld().getName();
-                int X = loc.getBlockX();
-                int Y= loc.getBlockY();
-                int Z = loc.getBlockZ();
-                float pitch = loc.getPitch();
-                float yaw = loc.getYaw();
+            final Player target = (Player) p;
+            Location loc = target.getLocation();
+            String world = loc.getWorld().getName();
+            int X = loc.getBlockX();
+            int Y = loc.getBlockY();
+            int Z = loc.getBlockZ();
+            String cat = args[1];
+            String warp = args[2];
+            FileConfiguration data = Files.DATA.getFile();
 
-                if (!(p instanceof Player)) {
-                    p.sendMessage(Methods.getPrefix() + Methods.getConsole());
-                    return true;
-                }
-                if (!Methods.hasPermission(p, "setwarp", true)) {
-                    return true;
-                }
-                if (args.length == 1 || args.length == 0) {
-                    p.sendMessage(Methods.getPrefix() + "&cPlease do /setwarp <category> <warp name>");
-                }
+            if(!(p instanceof Player)) {
+                p.sendMessage(Methods.getPrefix() + Methods.getConsole());
+                return true;
             }
+            if (!Methods.hasPermission(p, "setwarp", true)) {
+                return true;
+            }
+            if(args.length <= 2) {
+                p.sendMessage(Methods.getPrefix() + Methods.getWarp());
 
+            }
+            if (args.length == 3) {
+                data.set("categories." + cat + "." + warp + ".world", world);
+                data.set("categories." + cat + "." + warp + ".X",loc.getBlockX());
+                data.set("categories." + cat + "." + warp + ".Z", loc.getBlockZ());
+                data.set("categories." + cat + "." + warp + ".Y", loc.getBlockY());
+                data.set("categories." + cat + "." + warp + ".Pitch", loc.getPitch());
+                data.set("categories." + cat + "." + warp + ".Yaw", loc.getYaw());
+                Files.DATA.saveFile();
+                p.sendMessage(Methods.getPrefix() + Methods.setWarp());
+                return true;
+            }
         }
-        return false;
+    }
+        return true;
     }
 }
