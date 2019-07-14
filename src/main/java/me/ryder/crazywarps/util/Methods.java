dbda09faf36022312eeb402caba14b707df3fdc4
Ryder Belserion <Kolt0517@gmail.com>
2019-07-13 21:58:49 -0400

    redid configuration getters

--- src/main/java/me/ryder/crazywarps/util/Methods.java
+++ src/main/java/me/ryder/crazywarps/util/Methods.java
@@ -6,57 +6,75 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class Methods {
+    // Chat Color Conversion
     public static String pl(String M) {
         return ChatColor.translateAlternateColorCodes('&', M);
     }
-
-    public static String getPrefix(String string) {
+    // Prefix
+    public static String Prefix(String string) {
         return pl(Files.LANG.getFile().getString("messages.prefix") + string);
     }
 
     public static String getPrefix() {
-        return getPrefix("");
+        return Prefix("");
     }
-
+    // No Perms Message
     public static String getPerms(String string) {
         return pl(Files.LANG.getFile().getString("messages.no-perms") + string);
     }
 
-    public static String getPerms() {
+    public static String noPerms() {
         return getPerms("");
     }
-
+    // Plugin Reload Message
     public static String getReload(String string) {
-        return pl(Files.LANG.getFile().getString("messages.config-reload") + string);
+        return pl(Files.LANG.getFile().getString("messages.reload") + string);
     }
 
     public static String getReload() {
         return getReload("");
     }
-
+    // No Console means not allowed in console
     public static String getConsole(String string) {
-        return pl(Files.LANG.getFile().getString("messages.no-console") + string);
+        return pl(Files.LANG.getFile().getString("messages.player-only") + string);
     }
 
-    public static String getConsole() {
+    public static String noConsole() {
         return getConsole("");
     }
-
-    public static String getWarp(String string) {
-        return pl(Files.LANG.getFile().get("messages.set-warp") + string);
+    // Sent if they didn't use all the arguments in the command.
+    public static String warp1(String string) {
+        return pl(Files.LANG.getFile().getString("messages.create-warp") + string);
     }
 
-    public static String getWarp() {
-        return getWarp("");
+    public static String createWarp() {
+        return warp1("");
+    }
+    // Sent only if they created a warp successfully.
+    public static String warp2(String string) {
+        return pl(Files.LANG.getFile().getString("messages.created-warp") + string);
     }
 
-    public static String setWarp(String string) {
-        return pl(Files.LANG.getFile().get("messages.warp-set") + string);
+    public static String createdWarp() {
+        return warp2("");
     }
-    public static String setWarp() {
-        return setWarp("");
+    // Sent if they didn't use all the arguments in the command.
+    public static String warp3(String string) {
+        return pl(Files.LANG.getFile().getString("messages.delete-warp") + string);
     }
 
+    public static String deleteWarp() {
+        return warp3("");
+    }
+    // Sent only if they deleted a warp successfully.
+    public static String warp4(String string) {
+        return pl(Files.LANG.getFile().getString("messages.deleted-warp") + string);
+    }
+
+    public static String deletedWarp() {
+        return warp4("");
+    }
+    // Permission Checker
     public static boolean hasPermission(CommandSender p, String perm, Boolean toggle) {
         if (p instanceof Player) {
             return hasPermission((Player) p, perm, toggle);
@@ -64,12 +82,12 @@ public class Methods {
             return true;
         }
     }
-    public static boolean hasPermission(Player target, String perm, Boolean toggle) {
+    private static boolean hasPermission(Player target, String perm, Boolean toggle) {
         if (target.hasPermission("cw." + perm) || target.hasPermission("cw.admin")) {
             return true;
         } else {
             if (toggle) {
-                target.sendMessage(Methods.getPrefix() + Methods.getPerms());
+                target.sendMessage(Methods.getPrefix() + Methods.noPerms());
             }
             return false;
         }
