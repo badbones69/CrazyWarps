98a88e585e72ed95268bcdf3cf33c58bab1e76a2
Ryder Belserion <Kolt0517@gmail.com>
2019-07-14 00:52:17 -0400

    added a author check + extra Savefiles to turn into zipped backups soon

--- src/main/java/me/ryder/crazywarps/CrazyWarps.java
+++ src/main/java/me/ryder/crazywarps/CrazyWarps.java
@@ -1,11 +1,15 @@
 package me.ryder.crazywarps;
 
 import me.ryder.crazywarps.cmds.Commands;
+import me.ryder.crazywarps.util.Methods;
 import me.ryder.crazywarps.util.fm.FileManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.util.logging.Logger;
 import me.ryder.crazywarps.util.fm.FileManager.Files;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public final class CrazyWarps extends JavaPlugin {
 
@@ -60,6 +64,27 @@ public final class CrazyWarps extends JavaPlugin {
         m.info("");
         m.info("Crazy Warps " + getDescription().getVersion() + " shutting down.");
         m.info("");
+        m.info("Saving Files...");
+        saveFiles();
         m.info("-------------------------------");
     }
+    private void saveFiles() {
+        Files.CONFIG.saveFile();
+        Files.DATA.saveFile();
+        Files.LANG.saveFile();
+    }
+/*
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        Player t = e.getPlayer();
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (t.getName().equalsIgnoreCase("mitchy387")) {
+                    t.sendMessage(Methods.getPrefix() + Methods.pl("&cThis server is running CrazyWarps @ " + getDescription().getVersion() + "&7."));
+                }
+            }
+        }.runTaskLaterAsynchronously(this, 20);
+    }
+ */
 }
